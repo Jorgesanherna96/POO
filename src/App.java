2a1c800c3f480e7b9d04cb6484dbd5a999e96e12
jorgesanherna <jorgesanherna@usal.es>
2024-10-19 18:46:48 +0200

    Finalizado

--- src/App.java
+++ src/App.java
@@ -1,5 +1,86 @@
+
+import modelo.*;
+import com.coti.tools.Esdia;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+        
+        int opcion=0;
+        int tamano;
+        Almacen almacen= null;
+        int ritmo =1;
+        do {
+                System.out.println("|-------------------------------------------------------|");
+                System.out.println("|MIS LIBROS                                             |");
+                System.out.println("|-------------------------------------------------------|");
+                System.out.println("    1) Nuevo almacén de libros                  ");
+                System.out.println("    2) Establecer ritmo de lectura (páginas por minuto)  ");               
+                System.out.println("    3) Añadir un nuevo libro al almacen                   ");
+                System.out.println("    4) Mostrar información actual de libros             ");
+                System.out.println("    5) Salir (se borrará toda la información)              ");
+                System.out.println("|-------------------------------------------------------|");         
+
+                opcion= Esdia.readInt("Introduzca la opción: ",1,5);
+
+                switch(opcion){
+                    case 1:
+                        tamano=Esdia.readInt("Tamaño del almacen: ",1,50);
+                        almacen= new Almacen(tamano,ritmo);
+                        System.out.println("El almacen tiene una capacidad para: "+tamano);
+                        break;
+                    case 2: 
+                        ritmo=Esdia.readInt("Establece ritmo de lectura: ",1,10);
+                        almacen.setVelLectura(ritmo);
+                        break;
+                    
+                    case 3:
+                        if(almacen==null){
+                            System.out.println("No hay almacen :(");
+                        }
+                        else if(almacen.getCantLibros()>=almacen.getCapacidad()){
+                            System.out.println("El almacen esta lleno");
+
+                        }
+                        else{
+                            
+                            String nombreAutor=Esdia.readString("Introduce el nombre del autor:");
+                            String apellidosAutor=Esdia.readString("Introduce el apellido del autor:");
+                            boolean premioPlaneta=Esdia.siOno("Tiene premio planeta? ");
+
+                            Autor NewAutor= new Autor(nombreAutor,apellidosAutor,premioPlaneta);
+
+                            String tituloLibro=Esdia.readString("Introduce el tttulo del librin:");
+                            int anioPubli=Esdia.readInt("Introduce el año de publicacion",0,2024);
+                            int numeroPaginas=Esdia.readInt("Introduce el numero de paginas",0,10000);
+                            float price=Esdia.readFloat("introduce el precio",0,1000);
+
+                            Libro libro =new Libro(NewAutor,tituloLibro,anioPubli,numeroPaginas,price);
+                            almacen.añadirLibro(libro);
+                        }
+
+
+                        break;
+                    case 4:
+                         if(almacen==null){
+                        System.out.println("No hay almacen :(");
+                        }
+
+                        else{
+
+                            almacen.mostrar();
+
+
+                        }
+                        break;
+                    case 5:
+                          System.out.println("ADIOOOOOOOOS!!!");
+
+                }
+            }
+            while(opcion!=5);
+
+        }}
+
+
+       
--- src/modelo/Almacen.java
+++ src/modelo/Almacen.java
@@ -3,7 +3,77 @@ package modelo;
 public class Almacen {
     
 
-    public int tamaño;
-    public Libro[] libro;
+    private Libro[] libros;
+    private int CantLibros;
+    private int VelLectura;
+  
 
-}
+    public Almacen(){
+        
+    }
+  
+    public Almacen(int capacidad, int velLectura) {
+        libros = new Libro[capacidad];
+        CantLibros = 0;
+        this.VelLectura = velLectura;
+    }
+
+    public void setVelLectura(int velLectura) {
+        VelLectura = velLectura;
+    }
+
+    public int getCantLibros() {
+        return CantLibros;
+    }
+
+    public void añadirLibro(Libro newLibro) {
+
+        if(CantLibros<libros.length){
+
+            libros[CantLibros]=newLibro;
+            CantLibros++;
+            return;
+        }
+        System.out.println("Almacen full");
+        
+    }
+
+    public int getCapacidad(){
+
+
+        return libros.length;
+    }
+    
+    public void mostrar(){
+
+        if(CantLibros==0){
+            System.out.println("El almacén esta vacio. ");
+
+        }
+        float timepoTot=0;
+        float precioTotal=0;
+
+        System.out.println("|-----------------------------------------------------------------------------------------------------------------|");
+        System.out.println("| Título           | Año Publicación | Autor        | Premio Planeta | Páginas   | Tiempo lectura (min) | Precio  |");
+        System.out.println("|-----------------------------------------------------------------------------------------------------------------|");
+
+        for(int i =0;i<CantLibros;i++){
+
+            Libro libroActual=libros[i];
+            float tiempo= libroActual.getNumPag()/VelLectura;
+            timepoTot=timepoTot+tiempo;
+            precioTotal=precioTotal+libroActual.getPrecio();
+
+            System.out.printf("|%-18s | %-15d | %-10s %-10s | %-8s | %-7d | %-15.2f | %.2f $ | \n   ", 
+            libroActual.getTitulo(),libroActual.getAnioPublicacion(),(libroActual.getAutor()).getNombre(),(libroActual.getAutor()).getApellidos(),(libroActual.getAutor()).isPremioPlaneta(),libroActual.getNumPag(),tiempo,libroActual.getPrecio());
+
+        }
+        System.out.println("|--------------------------------------------------------------------------------------------------------------|");
+        System.out.println("|Tiempo de lectura total del almacén: " + timepoTot+ " min | Valor total del almacén: " + precioTotal+ " $       |");
+        System.out.println("|--------------------------------------------------------------------------------------------------------------|");
+
+        }
+        
+    }
+
+
